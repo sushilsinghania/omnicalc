590a5370bddd76b6decd582ff180308292365c2b
Sushil Kumar <sushilkumar@chicagobooth.edu>
2017-04-17 16:32:10 -0500

    Share with Uday

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -11,13 +11,25 @@ class CalculationsController < ApplicationController
     # ================================================================================
 
 
-    @word_count = "Replace this string with your answer."
 
-    @character_count_with_spaces = "Replace this string with your answer."
 
-    @character_count_without_spaces = "Replace this string with your answer."
+    @character_count_with_spaces = @text.length
 
-    @occurrences = "Replace this string with your answer."
+    text_wo_spaces = @text.gsub(" ","")
+    text_wo_linefeed = text_wo_spaces.gsub("\n","")
+    text_wo_cr = text_wo_linefeed.gsub("\r","")
+    text_wo_tabs = text_wo_cr.gsub("\t","")
+
+
+    @character_count_without_spaces = text_wo_tabs.length
+
+    text_array = @text.downcase.split
+
+    @word_count = text_array.length
+
+    @special_word = @special_word.gsub(" ","").gsub("\n","").gsub("\r","").gsub("\t","").downcase
+
+    @occurrences = text_array.count @special_word
 
     # ================================================================================
     # Your code goes above.
@@ -30,6 +42,8 @@ class CalculationsController < ApplicationController
     @apr = params[:annual_percentage_rate].to_f
     @years = params[:number_of_years].to_i
     @principal = params[:principal_value].to_f
+    monthly_rate = @apr/1200
+    number_periods = @years*12.0
 
     # ================================================================================
     # Your code goes below.
@@ -37,8 +51,7 @@ class CalculationsController < ApplicationController
     # The number of years the user input is in the integer @years.
     # The principal value the user input is in the decimal @principal.
     # ================================================================================
-
-    @monthly_payment = "Replace this string with your answer."
+    @monthly_payment = (monthly_rate*@principal)/(1-((1+monthly_rate)**(0-number_periods)))
 
     # ================================================================================
     # Your code goes above.
@@ -60,12 +73,12 @@ class CalculationsController < ApplicationController
     #   number of seconds as a result.
     # ================================================================================
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = @ending - @starting
+    @minutes = @seconds/60
+    @hours = @minutes/60
+    @days = @hours/24
+    @weeks = @days/7
+    @years = @days/365
 
     # ================================================================================
     # Your code goes above.
@@ -77,37 +90,86 @@ class CalculationsController < ApplicationController
   def descriptive_statistics
     @numbers = params[:list_of_numbers].gsub(',', '').split.map(&:to_f)
 
+    # //
+
     # ================================================================================
     # Your code goes below.
     # The numbers the user input are in the array @numbers.
     # ================================================================================
 
-    @sorted_numbers = "Replace this string with your answer."
+    @sorted_numbers = @numbers.sort
 
-    @count = "Replace this string with your answer."
+    @count = @numbers.count
 
-    @minimum = "Replace this string with your answer."
+    @minimum = @numbers.min
 
-    @maximum = "Replace this string with your answer."
+    @maximum = @numbers.max
 
-    @range = "Replace this string with your answer."
+    @range = @maximum - @minimum
 
-    @median = "Replace this string with your answer."
+    if @count %2 != 0
+      @median = @sorted_numbers[@count/2]
+    else
+      @median = (@sorted_numbers[@count/2]+@sorted_numbers[(@count/2)-1])/2.0
+    end
 
-    @sum = "Replace this string with your answer."
+    @sum = 0
+    counter = 0
+    while counter < (@count)
+      @sum = @sum + @sorted_numbers[counter]
+      counter = counter + 1
+    end
+    #
+    # @sum = "Replace this string with your answer."
 
-    @mean = "Replace this string with your answer."
+    @mean = @sum/(@count)
 
-    @variance = "Replace this string with your answer."
+    square_sum = 0
+    counter = 0
+    while counter < @count
+      square_sum = square_sum + (@sorted_numbers[counter] - @mean)**2
+      counter = counter + 1
+    end
 
-    @standard_deviation = "Replace this string with your answer."
+    @variance = square_sum/@count
 
-    @mode = "Replace this string with your answer."
+    @standard_deviation = @variance**0.5
+
+
+    # frequency = []
+    # counter1 = 0
+    #
+    # while counter1 < @count
+    #   counter2=0
+    #   while counter2 < (@count)
+    #
+    #     if @sorted_numbers[counter2+1] == @sorted_numbers[counter1]
+    #       counter2 = counter2+1
+    #     end
+    #   end
+    #   frequency[counter1] = counter2 + 1
+    #   counter1 = counter1 + 1
+    # end
+    #
+    # counter1 = 0
+    # max = 0
+    # while counter1<@count
+    #   if frequency[counter1+1] < frequency[counter1]
+    #     max = counter1
+    #   end
+    #   counter1 = counter1 + 1
+    # end
+
+
+    @mode = @sorted_numbers[0]
 
     # ================================================================================
     # Your code goes above.
     # ================================================================================
 
     render("descriptive_statistics.html.erb")
+
+
   end
+
 end
